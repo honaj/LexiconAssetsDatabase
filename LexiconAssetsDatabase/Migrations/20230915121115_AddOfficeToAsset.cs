8799488770516682d1fd9ed79f52f0fc4f5870a5
honaj <honajnahoj@gmail.com>
2023-09-15 16:27:33 +0200

    Init

---
+++ LexiconAssetsDatabase/Migrations/20230915121115_AddOfficeToAsset.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LexiconAssetsDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOfficeToAsset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Office",
+                table: "Assets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Office",
+                table: "Assets");
+        }
+    }
+}
---
+++ LexiconAssetsDatabase/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class Asset
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Office { get; set; }
+    public double Price { get; set; }
+    public DateTime PurchaseDate { get; set; }
+}
+
+public class AssetDbContext : DbContext
+{
+    public DbSet<Asset> Assets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Assets.db");
+    }
+
+    public async Task<List<Asset>> GetAllAssets()
+    {
+        return await Assets.ToListAsync();
+    }
+
+    public async Task CreateAsset(Asset newAsset)
+    {
+        Assets.Add(newAsset);
+        await SaveChangesAsync();
+    }
+    
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var dbContext = new AssetDbContext())
+        {
+            Console.WriteLine("Welcome to the asset manager");
+
+            // Add initial testing assets
+            dbContext.Assets.AddRange(
+                new Asset { Name = "Laptop", Office = "Miami", Price = 999.99, PurchaseDate = DateTime.Now },
+                new Asset { Name = "Mobile Phone", Office = "Madrid", Price = 499.99, PurchaseDate = DateTime.Now }
+            );
+
+            dbContext.SaveChanges();
+
+            // Continue with your application logic here
+        }
+    }
+}
