b5380237598c1b85c9513563b413dee1f8c16382
honaj <honajnahoj@gmail.com>
2023-09-18 18:57:04 +0200

    Fixes

--- LexiconAssetsDatabase/Program.cs
+++ LexiconAssetsDatabase/Program.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+public enum AssetType
+{
+    Phone,
+    Laptop
+}
+
 public class Asset
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Office { get; set; }
+    public string? Name { get; set; }
+    public string? Office { get; set; }
     public double Price { get; set; }
-    public DateTime PurchaseDate { get; set; }
+    public DateOnly PurchaseDate { get; set; }
+    public bool IsDeprecated => PurchaseDate < DateOnly.FromDateTime(DateTime.Now).AddYears(-2).AddMonths(-9);
+    public bool IsCloseToDeprecated => PurchaseDate < DateOnly.FromDateTime(DateTime.Now).AddYears(-2).AddMonths(-6);
+}
+
+public class Phone : Asset
+{
+    
+}
+
+public class Computer : Asset
+{
+    
 }
 
 public class AssetDbContext : DbContext
@@ -18,26 +36,26 @@ public class AssetDbContext : DbContext
         optionsBuilder.UseSqlite("Data Source=Assets.db");
     }
 
-    public async Task<List<Asset>> GetAllAssets()
+    public async Task<List<Asset?>> GetAllAssets()
     {
         return await Assets.ToListAsync();
     }
     
     // C
-    public async Task CreateAsset(Asset newAsset)
+    public async Task CreateAsset(Asset? newAsset)
     {
         Assets.Add(newAsset);
         await SaveChangesAsync();
     }
 
     // R
-    public async Task<Asset> GetAssetByID(int assetId)
+    public async Task<Asset?> GetAssetById(int assetId)
     {
         return await Assets.FindAsync(assetId);
     }
     
     // U
-    public async Task UpdateAsset(Asset updatedAsset)
+    public async Task UpdateAsset(Asset? updatedAsset)
     {
         if(updatedAsset is null) 
             return;
@@ -50,7 +68,7 @@ public class AssetDbContext : DbContext
     // D
     public async Task DeleteAsset(int assetId)
     {
-        var assetToDelete = await GetAssetByID(assetId);
+        var assetToDelete = await GetAssetById(assetId);
         
         if(assetToDelete is null)
             return;
@@ -72,8 +90,12 @@ class Program
         {
             // Add initial testing assets if there are none
             dbContext.Assets.AddRange(
-                new Asset { Name = "Laptop", Office = "Miami", Price = 999.99, PurchaseDate = DateTime.Now },
-                new Asset { Name = "Mobile Phone", Office = "Madrid", Price = 499.99, PurchaseDate = DateTime.Now }
+                new Computer { Name = "Asus", Office = "Miami", Price = 999.99, PurchaseDate = new DateOnly(2020, 01, 01) },
+                new Computer { Name = "Lenovo", Office = "Miami", Price = 999.99, PurchaseDate = new DateOnly(2021, 07, 15) },
+                new Computer { Name = "MacBook", Office = "Miami", Price = 999.99, PurchaseDate = new DateOnly(2022, 09, 01) },
+                new Phone { Name = "iPhone", Office = "Madrid", Price = 499.99, PurchaseDate = new DateOnly(2019, 05, 11) },
+                new Phone { Name = "Samsung", Office = "Madrid", Price = 499.99, PurchaseDate = new DateOnly(2023, 04, 18) },
+                new Phone { Name = "Nokia", Office = "Madrid", Price = 499.99, PurchaseDate = new DateOnly(2020, 11, 23) }
             );
 
             await dbContext.SaveChangesAsync();
@@ -83,7 +105,7 @@ class Program
         PrintAllAssets(allAssets);
     }
     
-    static void PrintAllAssets(List<Asset> assets)
+    static void PrintAllAssets(List<Asset?> assets)
     {
         if (assets.Count == 0)
         {
