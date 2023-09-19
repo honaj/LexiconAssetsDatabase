0608e74a3ac313b90ea5f079f5823b8f495fabd0
honaj <honajnahoj@gmail.com>
2023-09-19 11:32:49 +0200

    Nicer printing

--- LexiconAssetsDatabase/Program.cs
+++ LexiconAssetsDatabase/Program.cs
@@ -13,8 +13,6 @@ public class Asset
     public string? Office { get; set; }
     public double Price { get; set; }
     public DateOnly PurchaseDate { get; set; }
-    public bool IsDeprecated => PurchaseDate < DateOnly.FromDateTime(DateTime.Now).AddYears(-2).AddMonths(-9);
-    public bool IsCloseToDeprecated => PurchaseDate < DateOnly.FromDateTime(DateTime.Now).AddYears(-2).AddMonths(-6);
 }
 
 public class Phone : Asset
@@ -36,13 +34,13 @@ public class AssetDbContext : DbContext
         optionsBuilder.UseSqlite("Data Source=Assets.db");
     }
 
-    public async Task<List<Asset?>> GetAllAssets()
+    public async Task<List<Asset>> GetAllAssets()
     {
         return await Assets.ToListAsync();
     }
     
     // C
-    public async Task CreateAsset(Asset? newAsset)
+    public async Task CreateAsset(Asset newAsset)
     {
         Assets.Add(newAsset);
         await SaveChangesAsync();
@@ -55,7 +53,7 @@ public class AssetDbContext : DbContext
     }
     
     // U
-    public async Task UpdateAsset(Asset? updatedAsset)
+    public async Task UpdateAsset(Asset updatedAsset)
     {
         if(updatedAsset is null) 
             return;
@@ -105,7 +103,7 @@ class Program
         PrintAllAssets(allAssets);
     }
     
-    static void PrintAllAssets(List<Asset?> assets)
+    static void PrintAllAssets(IList<Asset?> assets)
     {
         if (assets.Count == 0)
         {
@@ -120,16 +118,45 @@ class Program
             "PURCHASE DATE",
             "EXPIRY DATE"
         );
+    
+        // Order the assets first by office then by purchase date
+        var orderedAssets = assets.OrderBy(a => a.Office).ThenBy(a => a.PurchaseDate);
 
-        foreach (var asset in assets)
+        foreach (var asset in orderedAssets)
         {
-            Console.WriteLine("{0,-20} {1,-20} {2,-20:C} {3,-20:d} {4,-20:d}",
+            // Check if the asset expiry date is less than 3 months or 6 months away from three years
+            var expiryDate = asset.PurchaseDate.AddYears(3);
+            var monthsToDeprecation = (expiryDate.Year - DateTime.Now.Year) * 12 + expiryDate.Month - DateTime.Now.Month;
+
+            Console.ForegroundColor = monthsToDeprecation switch
+            {
+                // Change the color of output text based on asset expiry date
+                <= 3 => ConsoleColor.Red,
+                <= 6 => ConsoleColor.Yellow,
+                _ => Console.ForegroundColor
+            };
+
+            // Convert price into local currency
+            var localCurrency = ConvertToLocalCurrency(asset.Price, asset.Office);
+        
+            Console.WriteLine("{0,-20} {1,-20} {2,-20} {3,-20:d} {4,-20:d}",
                 asset.Office,
                 asset.Name,
-                asset.Price,
+                localCurrency,
                 asset.PurchaseDate,
-                asset.PurchaseDate.AddYears(3)
+                expiryDate
             );
+        
+            // Reset the console color
+            Console.ResetColor();
         }
+    }
+
+    static double ConvertToLocalCurrency(double price, string office)
+    {
+        // This method is a dummy method. You have to implement the logic to convert the currency based on the office location.
+        // The conversion rate will be different for each office depending on the country in which it is located.
+        // For now, the method returns the price as it is.
+        return price;
     }
 }
