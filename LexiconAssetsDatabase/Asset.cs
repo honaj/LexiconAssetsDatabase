2c23507074c3e5c632c18acf31d148702e0aeac2
honaj <honajnahoj@gmail.com>
2023-09-26 04:15:26 +0200

    Moved some stuff to separate files

---
+++ LexiconAssetsDatabase/Asset.cs
@@ -0,0 +1,8 @@
+public class Asset
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Office { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly PurchaseDate { get; set; }
+}
